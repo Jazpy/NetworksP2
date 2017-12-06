7953925ad3eb872d1858bbc0f726fafa4fd4ce08
Jazpy <jaz.medtre@gmail.com>
2017-12-06 08:07:34 -0600

    FSM implementation

--- src/client.py
+++ src/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-
 import socket
+from select import select
 
 def main():
     s = socket.socket()
@@ -9,10 +9,92 @@ def main():
 
     s.connect((host, port))
 
-    while True:
-        s.sendall((10).to_bytes(1, byteorder = 'big'))
-        print(str(s.recv(1024)))
+    # Send the initial code
+    print('Client:')
+    print('Sending code: 10')
+    reply = (10).to_bytes(1, byteorder = 'big')
+    s.sendall(reply)
 
+    # Persistent variables
+    state = 1
+    close_connection = False
+
+    while True:
+        
+        # Clean our variable
+        reply = b''
+
+        # Get the server's response
+        data = ''
+        code = -1
+
+        # Check to see if there is data to read
+        r, _, _ = select([s], [], [])
+        if r:
+            print('e')
+            data = s.recv(4096)
+            code = int.from_bytes(data[:1], byteorder = 'big')
+        print('Client:')
+        print('Received code: ' + str(code))
+
+        # Monster if to represent the FSM
+        if state == 1 and code == 20:
+
+            # Extract pokemon_id
+            pokemon_id = int.from_bytes(data[1:2], byteorder = 'big')
+
+            # Ask for user input
+            to_catch = input('Catch pokemon with id ' + str(pokemon_id) + '?')
+
+            # Build reply
+            if to_catch == 'y':
+                state = 3
+                reply = (30).to_bytes(1, byteorder = 'big')
+            else:
+                state = 7
+                reply = (31).to_bytes(1, byteorder = 'big')
+        elif state == 3:
+
+            # Check if we caught the pokemon
+            if code == 22:
+                pokemon_id = int.from_bytes(data[1:2], byteorder = 'big')
+                image_size = int.from_bytes(data[2:3], byteorder = 'big')
+                image = int.from_bytes(data[3:], byteorder = 'big')
+                print('You caught the pokemon with code ' + str(pokemon_id))
+
+                # Switch to termination state
+                state = 7
+            elif code == 21:
+                pokemon_id = int.from_bytes(data[1:2], byteorder = 'big')
+                attempts = int.from_bytes(data[2:3], byteorder = 'big')
+
+                # Build reply
+                print(str(attempts) + ' attempts remaining')
+                to_catch = input('Keep trying to catch id ' + str(pokemon_id) + '?')
+
+                if to_catch == 'y':
+                    state = 3
+                    reply = (30).to_bytes(1, byteorder = 'big')
+                else:
+                    state = 7
+                    reply = (31).to_bytes(1, byteorder = 'big')
+
+            elif code == 23:
+                print('Failed to capture pokemon')
+
+                # Switch to termination state
+                state = 7
+        elif state == 7:
+            close_connection = True
+            reply = (32).to_bytes(1, byteorder = 'big')
+
+        s.sendall(reply)
+
+        # Check if connection ended
+        if close_connection or not data:
+            break
+        
+    print('Client is closing connection')
     s.close
 
 if __name__ == '__main__': main()
--- src/communication.py
+++ src/communication.py
@@ -1,24 +1,101 @@
+from random import randint
+from select import select
+
+# Aux functions
+def list_to_bytes(l):
+    ret = b''
+
+    for n in l:
+        ret = ret + n.to_bytes(1, byteorder = 'big')
+
+    return ret
+
 # Main logic for communication
 
 def client_thread(connection, address):
     
-    # Set initial state
+    # Set persistent variables
     state = 0
+    attempts = 3
+    pokemon_id = 0
+    close_connection = False
 
     # Main thread loop
     while True:
 
-        # Receive from client
-        data = connection.recv(1024)
-        code = int.from_bytes(data[:2], byteorder = 'big')
+        # Receive from client, parse the code, and clean reply variable
+        data = ''
+        code = -1
 
-        reply = 'Received code: ' + str(code)
+        # Check to see if there is data to read
+        r, _, _ = select([connection], [], [])
+        if r:
+            data = connection.recv(4096)
+            code = int.from_bytes(data[:1], byteorder = 'big')
+
+        reply = b''
+
+        # Logging
+        print('Server:')
+        print('Received code:' + str(code))
+
+        # Monster if to represent the FSM
+        if state == 0 and code == 10:
+
+            # Switch state
+            state = 2
+
+            # We pick a pokemon id at random and send that, along
+            # with the corresponding code
+            pokemon_id = randint(0, 9)
+            reply = list_to_bytes([20, pokemon_id])
+
+        elif (state == 2 or state == 4) and code == 30:
+
+            # Check if client captured pokemon, 50% chance
+            caught = True
+            if randint(0, 9) >= 5:
+                caught = False
+
+            if caught:
+                state = 5
+
+                out_code = (22).to_bytes(1, byteorder = 'big')
+                pokemon_id_code = pokemon_id.to_bytes(1, byteorder = 'big')
+                image_size = 10
+                image_size_code = image_size.to_bytes(4, byteorder = 'big')
+                image_code = (0).to_bytes(image_size, byteorder = 'big')
+                reply = out_code + pokemon_id_code + image_size_code + image_code
+
+                # Cleanup
+                attempts = 3
+            else:
+                # Check if user is out of attempts
+                if attempts == 0:
+                    state = 6
+                    reply = list_to_bytes([23])
+
+                    # Cleanup
+                    attempts = 3
+                else:
+                    state = 4
+                    reply = list_to_bytes([21, pokemon_id, attempts])
+
+            attempts = attempts - 1
+        elif (state == 2 or state == 4) and code == 31:
+            state = 6
+        elif (state == 5 or state == 6) and code == 32:
+            close_connection = True
+            reply = list_to_bytes([32])
+
+        # Send the message
+        print('Server:')
+        print('Sending code: ' + str(reply))
+        connection.sendall(reply)
 
         # Check if connection ended
-        if not data:
+        if close_connection or not data:
             break
-
-        connection.sendall(reply.encode('utf-8'))
 
     connection.close()
     print('Closed connection to ' + address[0] + ':' + str(address[1]))
